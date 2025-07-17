c0ef174420b568965c29b0e876a35a70838bea21
Gonçalo Quesado <goncaloquesado@gmail.com>
2025-07-17 09:57:42 +0100

    Commit 1 - Base

---
+++ src/components/FactoryMap.jsx
@@ -0,0 +1,11 @@
+function FactoryMap() {
+    return (
+      <div>
+        <h2>Factory Layout</h2>
+        {/* Canvas do Konva vai aparecer aqui */}
+      </div>
+    );
+  }
+  
+  export default FactoryMap;
+  
